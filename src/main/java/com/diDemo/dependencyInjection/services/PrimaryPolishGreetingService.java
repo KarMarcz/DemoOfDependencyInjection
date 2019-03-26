df1f575ceb9f911bb652ab712d7443ecaad4dfa1
Karol Marczyk <KarMarcz@gmail.com>
2019-03-26 20:05:07 +0100

    Profile adnotation

--- src/main/java/com/diDemo/dependencyInjection/services/PrimaryPolishGreetingService.java
+++ src/main/java/com/diDemo/dependencyInjection/services/PrimaryPolishGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("pl")
 @Primary
-@Profile("en")
-public class PrimaryGreetingservice implements GreetingService {
+public class PrimaryPolishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - from primary";
+        return "Siemanko z serwisu Polskiego";
     }
 }
