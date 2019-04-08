0087bf16bd94a47e99570185cb859fe395a4c801
Karol Marczyk <KarMarcz@gmail.com>
2019-04-08 21:13:01 +0200

    Spring Enviroments Properties Override

--- src/main/java/com/diDemo/dependencyInjection/config/PropertyConfig.java
+++ src/main/java/com/diDemo/dependencyInjection/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.diDemo.dependencyInjection.config;
 
 import com.diDemo.dependencyInjection.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${username}")
 //    @Value("username")
@@ -26,7 +31,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME_USER"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
